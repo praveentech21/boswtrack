b0eecb3f96404d57ed9b0a23a258e4aaac10ed10
Sai Praveen <praveen21.techgmail.com>
2025-12-17 07:54:56 +0530

    connection updated

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli('localhost','root','','lunchbox');
+$con = new mysqli('localhost','root','','boswtrack');
 //databse connection checking shiva
 ?>
 <!-- All Setted Shiva and this is sending to git as All set SHiva -->   
